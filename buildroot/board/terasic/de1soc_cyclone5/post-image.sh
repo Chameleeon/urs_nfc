ce0923609923c988c155895e8631ea6b7248acf4
Danilo Todorović <45533047+Chameleeon@users.noreply.github.com>
2025-09-16 13:03:38 +0200

    Update post-image.sh

--- buildroot/board/terasic/de1soc_cyclone5/post-image.sh
+++ buildroot/board/terasic/de1soc_cyclone5/post-image.sh
@@ -3,3 +3,4 @@
 
 # Copy socfpga.rbf to the boot partition
 cp ${BR2_EXTERNAL_PATH:-board/terasic/de1soc_cyclone5}/socfpga.rbf ${BINARIES_DIR}/
+cp ${BR2_EXTERNAL_PATH:-board/terasic/de1soc_cyclone5}/boot-env.txt ${BINARIES_DIR}/
